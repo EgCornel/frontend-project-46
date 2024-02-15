8b8a28bf216897820a816e46d0dec4eb2245e301
EgCornel <egor.liney@gmail.com>
2024-02-15 13:48:12 +0300

    step 3

--- src/parsing.js
+++ src/parsing.js
@@ -1,15 +1,17 @@
 import * as fs from 'node:fs';
 import path from 'path';
 
-const getPath = ('filepath') => path.resolve(process.cwd(), 'filepath');
+const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const genDiff = (filepath1, filepath2) => {
   const resolveFilepath1 = getPath(filepath1);
   const resolveFilepath2 = getPath(filepath2);
-  const Cont1 = fs.readFileSync(resolveFilepath1, { encoding: 'utf-8' });
-  const Cont2 = fs.readFileSync(resolveFilepath2, { encoding: 'utf-8' });
-  const parser = JSON.parse(Cont1);
-  return parser;
+  const cont1 = fs.readFileSync(resolveFilepath1, { encoding: 'utf-8' });
+  const cont2 = fs.readFileSync(resolveFilepath2, { encoding: 'utf-8' });
+  const parser1 = JSON.parse(cont1);
+  const parser2 = JSON.parse(cont2);
+  console.log(parser1);
+  console.log(parser2);
 };
 
 export default genDiff;
